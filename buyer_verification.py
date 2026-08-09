24d400ae92443373e7dee79d9325e2adbb4927a0
dolambikaachyutha <282688662+dolambikaachyutha@users.noreply.github.com>
2026-08-09 17:27:39 +0530

    Update buyer_verification.py

--- buyer_verification.py
+++ buyer_verification.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from db.db_utils import get_all_verifications
+from db_utils import get_all_verifications
 
 # Ensure user is logged in
 if not st.session_state.user or st.session_state.user["role"] != "buyer":
